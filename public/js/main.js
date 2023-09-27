74f2708cdcc777f80479c67ac59799a1b67d8c6d
Archil Gogia <arshileg@gmail.com>
2023-09-27 21:13:09 +0400

    Optimizes functions

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,12 @@
 //Generates random whole number in Range
-function randomizeNumber(min, max) {
+function getRandomNumber(min, max) {
   // If min/max have equal values returns min
   if(max === min) {
     return min;
   }
   // If min is a larger number than max, their positions are swapped for correct calculation
   if(min > max) {
-    const temp = min;
-    min = max;
-    max = temp;
+    return randomizeNumber(max, minß);
   }
   // If any argument is less than 0,
   //the function calls itself recursively with the argument transformed using Math.abs().
@@ -21,24 +19,8 @@ function randomizeNumber(min, max) {
 
 // Generates random floating number in Range
 // with an argument to specify number of decimal digits
-function randomizeDecimals(min, max, decimals) {
-  // If min/max have equal values returns min
-  if(max === min) {
-    return min;
-  }
-  // If min is a larger number than max, their positions are swapped for correct calculation
-  if(min > max) {
-    const temp = min;
-    min = max;
-    max = temp;
-  }
-  // If any argument is less than 0,
-  // the function calls itself recursively with the argument transformed using Math.abs().
-  if(min < 0 || max < 0) {
-    return randomizeDecimals(Math.abs(min), Math.abs(max));
-  }
-
-  const randomNum =  Math.random() * (max - min + 1) + min;
+function getRandomNumberDecimal(min, max, decimals) {
+  const randomNum =  getRandomNumer(min,max Boolean(decimal);
   return randomNum.toFixed(decimals);
 }
 
