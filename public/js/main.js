a5a044b05c0170ce6b6b48ef9d16855ba9aafede
Archil Gogia <arshileg@gmail.com>
2023-09-27 18:55:06 +0400

    Adds another function and fixes previous one

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,46 @@
 //Generates random whole number in Range
-function randomizerInRange(min, max) {
-  if (min >= max || min < 0 || max < 0) {
-    return 'Error: Invalid input. min should be less than max, and both should be non-negative.';
+function randomizeNumber(min, max) {
+  // If min/max have equal values returns min
+  if(max === min) {
+    return min;
   }
+  // If min is a larger number than max, their positions are swapped for correct calculation
+  if(min > max) {
+    const temp = min;
+    min = max;
+    max = temp;
+  }
+  // If any argument is less than 0,
+  //the function calls itself recursively with the argument transformed using Math.abs().
+  if(min < 0 || max < 0) {
+    return randomizeNumber(Math.abs(min), Math.abs(max));
+  }
+
   return  Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-randomizerInRange(1, 2);
+// Generates random floating number in Range
+// with an argument to specify number of decimal digits
+function randomizeDecimals(min, max, decimals) {
+  // If min/max have equal values returns min
+  if(max === min) {
+    return min;
+  }
+  // If min is a larger number than max, their positions are swapped for correct calculation
+  if(min > max) {
+    const temp = min;
+    min = max;
+    max = temp;
+  }
+  // If any argument is less than 0,
+  // the function calls itself recursively with the argument transformed using Math.abs().
+  if(min < 0 || max < 0) {
+    return randomizeDecimals(Math.abs(min), Math.abs(max));
+  }
+
+  const randomNum =  Math.random() * (max - min + 1) + min;
+  return randomNum.toFixed(decimals);
+}
+
+randomizeNumber(1, 5);
+randomizeDecimals(1, 5, 3);
