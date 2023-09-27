da54037a4e6926477a4e837ef805569d44955342
Archil Gogia <arshileg@gmail.com>
2023-09-27 21:44:37 +0400

    Final optimization

--- public/js/main.js
+++ public/js/main.js
@@ -1,28 +1,28 @@
-//Generates random whole number in Range
-function getRandomNumber(min, max) {
+//Generates random number in range
+const getRandomNumber = function(min, max) {
   // If min/max have equal values returns min
   if(max === min) {
     return min;
   }
   // If min is a larger number than max, their positions are swapped for correct calculation
   if(min > max) {
-    return randomizeNumber(max, min);
+    return getRandomNumber(max, min);
   }
   // If any argument is less than 0,
   //the function calls itself recursively with the argument transformed using Math.abs().
   if(min < 0 || max < 0) {
-    return randomizeNumber(Math.abs(min), Math.abs(max));
+    return getRandomNumber(Math.abs(min), Math.abs(max));
   }
 
-  return  Math.floor(Math.random() * (max - min + 1)) + min;
-}
+  return  Math.random() * (max - min + 1) + min;
+};
 
-// Generates random floating number in Range
+//Generates random whole number in range
+const getRandomInteger = (min, max) => Math.floor(getRandomNumber(min, max));
+
+// Generates random floating number in range
 // with an argument to specify number of decimal digits
-function getRandomNumberDecimal(min, max, decimals) {
-  const randomNum =  getRandomNumer(min,max Boolean(decimal);
-  return randomNum.toFixed(decimals);
-}
+const getRandomFloatString = (min, max, decimal) => getRandomNumber(min,max).toFixed(decimal);
 
-randomizeNumber(1, 5);
-randomizeDecimals(1, 5, 3);
+getRandomInteger(1, 5);
+getRandomFloatString(1, 5, 3);
